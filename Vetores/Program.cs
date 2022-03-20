71a133e58762abcd7d612e806db1939c35ee38b8
Leolopes013 <leo_piula@hotmail.com>
2022-03-20 20:52:39 -0300

    Vetor commit

---
+++ Vetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            int i = 0;
+            //Cadastro dos nomes
+            for (int x = 0; x < 10; x++)
+            {
+                Console.Write("Informe o "+ (x + 1) +"o. nome..: " );
+                nomes[x] = Console.ReadLine();
+            }
+
+            Console.Clear();  // limpa a tela do console
+
+            //impressão dos nomes
+            while (i < 10)
+            {
+                Console.WriteLine(nomes[i]);
+                i++;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
